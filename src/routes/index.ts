c021ecf803accc0bb6261f4520fc4fcb7098f241
MuryloSaladino <murylosaladino@gmail.com>
2025-06-02 00:34:10 -0300

    feat: starter route setup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,24 +1,41 @@
 import { createBrowserRouter } from "react-router";
-import { AppRoutes } from "../constants/app-routes";
-import { AppShell } from "../components/layout/app-shell/index.tsx";
+import { AppRoutes } from "../constants/app-routes.ts";
+import { AppLayout } from "../components/layout/app-layout/index.tsx";
 import { Login } from "../pages/login/index.tsx";
-import { Home } from "../pages/home/index.tsx";
+import { Root } from "../pages/root/index.tsx";
+import { App } from "../App.tsx";
+import { AuthenticationGuard } from "../components/guards/authentication/index.tsx";
+import { Dashboard } from "../pages/dashboard/index.tsx";
 
 export const router = createBrowserRouter([{
+    Component: App,
     children: [
         {
+            // Redirect to root when path not found
+            path: "*",
+            Component: Root
+        },
+        {
+            // Root is not implemented, so it just redirects to dashboard
+            path: AppRoutes.ROOT,
+            Component: Root
+        },
+        {
+            // Login page is open, without guards
             path: AppRoutes.LOGIN,
             Component: Login,
         },
         {
-            Component: AppShell,
+            // Component without path => layout
+            Component: AppLayout,
             children: [{
+                // Guard for authenticated routes
+                Component: AuthenticationGuard,
                 children: [
                     {
-                        path: AppRoutes.ROOT,
-                        Component: Home
-                    }
-                    
+                        path: AppRoutes.DASHBOARD,
+                        Component: Dashboard
+                    },
                 ]
             }]
         },
