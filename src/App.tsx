a38beeec514a1e9cade498eb8b0ca235ebf6237c
MuryloSaladino <murylosaladino@gmail.com>
2025-06-02 00:35:02 -0300

    feat: app with mantine and user provider plus notifier container

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import '@mantine/core/styles.css';
+
+import { Outlet } from "react-router";
+import { UserContextProvider } from "./contexts/user.context";
+import { MantineProvider } from "@mantine/core";
+import { Notifier } from './components/feedback/notifier';
+
+export function App() {
+    return(
+        <MantineProvider defaultColorScheme="dark">
+            <UserContextProvider>
+                <Outlet/>
+                <Notifier/>
+            </UserContextProvider>
+        </MantineProvider>
+    )
+}
