d7eea89269e1a64c4fdcb8c31f914142e4ae34a2
MuryloSaladino <murylosaladino@gmail.com>
2025-06-02 00:35:14 -0300

    feat: router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+    <RouterProvider router={router} />
 )
