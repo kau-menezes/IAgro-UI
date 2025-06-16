a912772c5a6474de5af283245d2da9b8df0f37f2
MuryloSaladino <murylosaladino@gmail.com>
2025-06-15 23:56:21 -0300

    feat: leaflet css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import '@mantine/core/styles.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
 
 import { Outlet } from "react-router";
 import { UserContextProvider } from "./contexts/user.context";
 import { MantineProvider } from "@mantine/core";
 import { Notifier } from './components/feedback/notifier';
+import { theme } from './mantine/theme';
 
 export function App() {
     return(
-        <MantineProvider>
+        <MantineProvider theme={theme}>
             <UserContextProvider>
                 <Outlet/>
                 <Notifier/>
