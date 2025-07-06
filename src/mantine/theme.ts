d3f08b000d967ce251323f2b47c3e6b2a95e2116
MuryloSaladino <murylosaladino@gmail.com>
2025-07-06 17:47:08 -0300

    feat: green brand colors

--- src/mantine/theme.ts
+++ src/mantine/theme.ts
@@ -1,5 +1,8 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-    fontFamily: "'Josefin Sans', sans-serif"
+    fontFamily: "'Josefin Sans', sans-serif",
+    colors: {
+        green: ["#52A260", "#52A260", "#52A260", "#52A260", "#52A260", "#52A260", "#52A260", "#52A260", "#52A260", "#52A260"]
+    }
 })
